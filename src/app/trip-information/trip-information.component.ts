a627980616239ddc8f50dde000677e67843799fa
Exchange_App <gohnphilip2000@gmail.com>
2020-05-31 16:06:24 +0200

    project overview

--- src/app/trip-information/trip-information.component.ts
+++ src/app/trip-information/trip-information.component.ts
@@ -5,34 +5,66 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './trip-information.component.html',
   styleUrls: ['./trip-information.component.css']
 })
+
+
 export class TripInformationComponent implements OnInit {
-  isEmpty = true;
-  price = "350";
-  numOfRivew = "220";
-  numOfDays = '5 ';
-  place = "Africia";
-  fromDate = "15/5/2020";
-  ToDate = "20/5/2020";
+ 
+
+
+
+
+
+tripsInfo=[{
+id:"10",
+Like:true,
+price:"550",
+numOfRivew:"220",
+place:"Africia",
+fromDate:"15/5/2020",
+toDate:"20/5/2020",
+numOfDays :'5 ',
+rate:3,
+TripDetail1 : "2 Day at conrad",
+TripDetail2 : "1 Day at Desert",
+TripDetail3 : "4 Days at pyramid visite with camel",
+TripDetail4 : "1 Days at wadi el hitan bed and brackfast",
+},
+{
+  id:"10",
+  Like:false,
+  price:"350",
+  numOfRivew:"220",
+  place:"Africia",
+  fromDate:"15/5/2020",
+  toDate:"20/5/2020",
+  numOfDays :'5 ',
+  rate:3,
+  TripDetail1 : "2 Day at conrad",
+  TripDetail2 : "1 Day at Desert",
+  TripDetail3 : "4 Days at pyramid visite with camel",
+  TripDetail4 : "1 Days at wadi el hitan bed and brackfast",
+  }]
+ 
+isEmpty=this.tripsInfo[1].Like;
+rate=this.tripsInfo[0].rate;
   
-  TripDetails=[
-    {TripDetail : "2 Day at conrad"},
-    {TripDetail : "1 Day at Desert"},
-    {TripDetail : "4 Days at pyramid visite with camel"},
-    {TripDetail : "1 Days at wadi el hitan bed and brackfast"}
-  ]
+ 
   
   max: number = 5;
-  rate: number = 4;
+  
   isReadonly: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
   }
   liked() {
-    if (this.isEmpty === true) {
+    if (this.isEmpty=== true) {
       this.isEmpty = false;
     }
     else { this.isEmpty = true }
 
   }
+ 
+    
+  
 }
